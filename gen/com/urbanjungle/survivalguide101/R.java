0440ad58292568f4303ac743dc1eaf75ddc5d1b0
fernandolicon <wisimata@gmail.com>
2013-11-08 13:37:37 -0700

    Diseno Splash

--- gen/com/urbanjungle/survivalguide101/R.java
+++ gen/com/urbanjungle/survivalguide101/R.java
@@ -27,28 +27,30 @@ public final class R {
         public static final int metro_1=0x7f020003;
         public static final int peligroso_1=0x7f020004;
         public static final int sf=0x7f020005;
-        public static final int splashimage=0x7f020006;
+        public static final int soslatino=0x7f020006;
+        public static final int soslatino06=0x7f020007;
+        public static final int soslatinocopia=0x7f020008;
+        public static final int splashimage=0x7f020009;
     }
     public static final class id {
-        public static final int TextView01=0x7f080007;
-        public static final int action_settings=0x7f080012;
-        public static final int btnLA=0x7f080001;
-        public static final int btnSF=0x7f080002;
-        public static final int imageView1=0x7f080000;
-        public static final int imgImagen=0x7f080010;
-        public static final int lstHome=0x7f080004;
-        public static final int lstTraducc=0x7f080006;
-        public static final int textView2=0x7f08000c;
-        public static final int textView5=0x7f08000d;
-        public static final int txTitol=0x7f080003;
-        public static final int txtAddr=0x7f08000b;
-        public static final int txtDescr=0x7f080008;
-        public static final int txtEspanol=0x7f080011;
-        public static final int txtEspecialidad=0x7f08000e;
-        public static final int txtImagen=0x7f08000a;
-        public static final int txtIngl=0x7f080005;
-        public static final int txtSubTit=0x7f080009;
-        public static final int txtTelefono=0x7f08000f;
+        public static final int TextView01=0x7f080006;
+        public static final int action_settings=0x7f080011;
+        public static final int btnLA=0x7f080000;
+        public static final int btnSF=0x7f080001;
+        public static final int imgImagen=0x7f08000f;
+        public static final int lstHome=0x7f080003;
+        public static final int lstTraducc=0x7f080005;
+        public static final int textView2=0x7f08000b;
+        public static final int textView5=0x7f08000c;
+        public static final int txTitol=0x7f080002;
+        public static final int txtAddr=0x7f08000a;
+        public static final int txtDescr=0x7f080007;
+        public static final int txtEspanol=0x7f080010;
+        public static final int txtEspecialidad=0x7f08000d;
+        public static final int txtImagen=0x7f080009;
+        public static final int txtIngl=0x7f080004;
+        public static final int txtSubTit=0x7f080008;
+        public static final int txtTelefono=0x7f08000e;
     }
     public static final class layout {
         public static final int activity_cities=0x7f030000;
