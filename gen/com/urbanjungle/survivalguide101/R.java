19e41c927ee586f7f27bb2cf6d0184a767f853da
fernandolicon <wisimata@gmail.com>
2013-11-08 16:17:33 -0700

    App con Bookmarks (read only)

--- gen/com/urbanjungle/survivalguide101/R.java
+++ gen/com/urbanjungle/survivalguide101/R.java
@@ -34,23 +34,27 @@ public final class R {
     }
     public static final class id {
         public static final int TextView01=0x7f080006;
-        public static final int action_settings=0x7f080011;
+        public static final int action_settings=0x7f080015;
+        public static final int btnAdd=0x7f080011;
+        public static final int btnAgregar=0x7f08000a;
+        public static final int btnDelete=0x7f080012;
         public static final int btnLA=0x7f080001;
         public static final int btnSF=0x7f080000;
-        public static final int imgImagen=0x7f08000f;
+        public static final int bttnQuitar=0x7f08000b;
+        public static final int imgImagen=0x7f080013;
         public static final int lstHome=0x7f080003;
         public static final int lstTraducc=0x7f080005;
-        public static final int textView2=0x7f08000b;
-        public static final int textView5=0x7f08000c;
+        public static final int textView2=0x7f08000d;
+        public static final int textView5=0x7f08000e;
         public static final int txTitol=0x7f080002;
-        public static final int txtAddr=0x7f08000a;
+        public static final int txtAddr=0x7f08000c;
         public static final int txtDescr=0x7f080007;
-        public static final int txtEspanol=0x7f080010;
-        public static final int txtEspecialidad=0x7f08000d;
+        public static final int txtEspanol=0x7f080014;
+        public static final int txtEspecialidad=0x7f08000f;
         public static final int txtImagen=0x7f080009;
         public static final int txtIngl=0x7f080004;
         public static final int txtSubTit=0x7f080008;
-        public static final int txtTelefono=0x7f08000e;
+        public static final int txtTelefono=0x7f080010;
     }
     public static final class layout {
         public static final int activity_cities=0x7f030000;
